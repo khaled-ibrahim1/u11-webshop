7a5d6b067d7d03c5d4526a93359ef1b3cfde6cd1
khaled-ibrahim1 <72127751+khaled-ibrahim1@users.noreply.github.com>
2022-05-31 04:16:38 +0200

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const cors = require("cors")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productsRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const stripeRoute = require("./routes/stripe");
+
+
+
+
+
+
+
+mongoose.connect(
+    process.env.MONGO_URL)
+.then(()=>console.log("dbconnected"))
+.catch((err) =>{
+    console.log(err);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/products", productsRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/checkout", stripeRoute);
+
+
+
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running!");
+  });
